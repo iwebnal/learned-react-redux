7d33fab42e7b87a82ae2022ee96857c607ecde61
iwebnal <iwebnal@gmail.com>
2024-12-26 17:15:30 +0300

    React-Redux app structure final final

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,9 +2,8 @@ import React from 'react';
 import './app.css';
 import { withBookstoreService } from '../hoc';
 
-const App = ({ bookstoreService }) => {
-    console.log(bookstoreService.getBooks());
+const App = () => {
     return <div>App Component</div>
 };
 
-export default withBookstoreService()(App);
+export default App;
