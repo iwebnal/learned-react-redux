ba00a0a4fa0f1b61604357b4e43c1fb391fc2860
iwebnal <iwebnal@gmail.com>
2024-12-26 20:19:09 +0300

    Read to data Redux Store

--- src/components/pages/home-page.js
+++ src/components/pages/home-page.js
@@ -2,37 +2,8 @@ import React from 'react';
 import BookList from '../book-list';
 
 const HomePage = () => {
-    const books = [
-        {
-            id: 1,
-            title: 'Test book 1',
-            author: 'Test author 1'
-        },
-        {
-            id: 2,
-            title: 'Test book 2',
-            author: 'Test author 2'
-        },
-        {
-            id: 3,
-            title: 'Test book 3',
-            author: 'Test author 3'
-        },
-        {
-            id: 4,
-            title: 'Test book 4',
-            author: 'Test author 4'
-        },
-        {
-            id: 5,
-            title: 'Test book 5',
-            author: 'Test author 5'
-        },
-    ]
-
-    // return <div>Home Page</div>
     return (
-        <BookList books={books} />
+        <BookList />
     )
 }
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,34 @@
+const books = [
+        {
+            id: 1,
+            title: 'Test book 1',
+            author: 'Test author 1'
+        },
+        {
+            id: 2,
+            title: 'Test book 2',
+            author: 'Test author 2'
+        },
+        {
+            id: 3,
+            title: 'Test book 3',
+            author: 'Test author 3'
+        },
+        {
+            id: 4,
+            title: 'Test book 4',
+            author: 'Test author 4'
+        },
+        {
+            id: 5,
+            title: 'Test book 5',
+            author: 'Test author 5'
+        },
+    ]
 
 const initialState = {
-    books: []
+    // books: [],
+    books: books // тут находятся даные из Redux Store для компонента BookList который обернут в connect. В файле book-list.js
 };
 
 const reducer = (state = initialState, action) => {
